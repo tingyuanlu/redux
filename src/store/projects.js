db5f328c45c5ae77e60f6fd2337c0563fa036eea
tingyuanlu <tingyuan.lu43@gmail.com>
2020-11-03 13:37:35 -0500

    create slicer

--- src/store/projects.js
+++ src/store/projects.js
@@ -1,22 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 let lastId = 0;
 const slice = createSlice({
-  name: "bugs",
+  name: "projects",
   initialState: [],
   reducers: {
-    bugAdded: (bugs, action) => {
-      bugs.push({
+    //action=>action handler
+    projectAdded: (projects, action) => {
+      projects.push({
         id: ++lastId,
         description: action.payload.description,
         resolved: false,
       });
     },
-    bugResolved: (bugs, action) => {
-      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-      bugs[index].resolved = true;
-    },
   },
 });
 
-export const { bugAdded, bugResolved } = slice.actions;
+export const { projectAdded } = slice.actions;
 export default slice.reducer;
