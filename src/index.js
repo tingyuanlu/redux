cd313e9f4229de1f857f0b99e3247feb4fb6bb5d
tingyuanlu <tingyuan.lu43@gmail.com>
2020-11-04 19:35:52 -0500

    combining Reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import configureStore from "./store/configureStore";
 import * as actions from "./store/bugs";
-
+import { projectAdded } from "./store/projects";
 const store = configureStore();
 
 store.subscribe(() => {
   console.log("Store change");
 });
+store.dispatch(projectAdded({ name: "Porject1" }));
 store.dispatch(actions.bugAdded({ discription: "bug1" }));
 store.dispatch(actions.bugAdded({ discription: "bug2" }));
 store.dispatch(actions.bugAdded({ discription: "bug3" }));
