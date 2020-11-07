20b1ae76d8feb85ba84f90990fb006b4b910423d
tingyuanlu <tingyuan.lu43@gmail.com>
2020-11-07 15:00:37 -0500

    middleware

--- src/index.js
+++ src/index.js
@@ -15,17 +15,6 @@ store.subscribe(() => {
 });
 
 store.dispatch(userAdded({ name: "user1" }));
-store.dispatch(userAdded({ name: "user2" }));
 
-store.dispatch(projectAdded({ name: "Porject1" }));
-store.dispatch(bugAdded({ discription: "bug1" }));
-store.dispatch(bugAdded({ discription: "bug2" }));
-store.dispatch(bugAdded({ discription: "bug3" }));
-store.dispatch(bugAssignedToUser({ userId: 1, bugId: 1 }));
-store.dispatch(bugResolved({ id: 1 }));
-const x = getUnresolveBugs(store.getState());
-const y = getUnresolveBugs(store.getState());
-
-console.log(x === y);
 const bugs = getBugsByUser(1)(store.getState());
 console.log(bugs);
